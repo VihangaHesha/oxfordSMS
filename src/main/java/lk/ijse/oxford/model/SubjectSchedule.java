503079c788fe27dcc36fd0fd6b6c94a445289e8e
Vihanga Heshan Bandara <heshanvihanga321@gmail.com>
2024-04-25 09:59:08 +0530

    some changes in fxml files

---
+++ src/main/java/lk/ijse/oxford/model/SubjectSchedule.java
@@ -0,0 +1,13 @@
+package lk.ijse.oxford.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SubjectSchedule {
+    private String TimeId;
+    private String EmpId;
+}
